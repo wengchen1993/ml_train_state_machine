7c16ea0e5479486db0211b82676cb98a3fd87e10
wengchen1993 <wengchen1993@gmail.com>
2020-12-16 05:10:55 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="",
+    version="0.1",
+    description="",
+    author="",
+    packages=find_packages()
+)
